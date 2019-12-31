436b135762347ceb4ccc07a4f4a05e8e9dd74383
Asija Watson <asija.w@yahoo.com>
2019-12-31 20:47:45 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,6 +1,10 @@
 class shoe
   attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
   
   def cobble
     puts "Your shoe is as good as new!"
+    shoe.condition = "new"
+  end
+end
     
